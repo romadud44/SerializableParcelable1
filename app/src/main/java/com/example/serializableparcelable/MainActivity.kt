df4f36c379c550f46ddc6c4b544e0b3538b53da2
romadud44 <roman_crt-service@mail.ru>
2024-08-15 19:02:51 +0300

    commit

--- app/src/main/java/com/example/serializableparcelable/MainActivity.kt
+++ app/src/main/java/com/example/serializableparcelable/MainActivity.kt
@@ -58,6 +58,8 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
